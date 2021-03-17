b6a9afe6074c7d281eb930dee728bf76ff44c452
RSTq1337 <37782356+RSTq1337@users.noreply.github.com>
2021-03-17 12:42:14 +0300

    Update city.service.ts
    added rest urls

--- src/app/city.service.ts
+++ src/app/city.service.ts
@@ -8,28 +8,28 @@ import { Observable } from 'rxjs';
 
 export class CityService {
 
-  private baseUrl = 'http://localhost:8080/api/';
+  private baseUrl = 'http://localhost:8080/api/city';
 
   constructor(private http:HttpClient) { }
 
   getCityList(): Observable<any> {
-    return this.http.get(`${this.baseUrl}`+'city-list');
+    return this.http.get(`${this.baseUrl}`);
   }
 
   createCity(city: object): Observable<object> {
-    return this.http.post(`${this.baseUrl}`+'save-city', city);
+    return this.http.post(`${this.baseUrl}`, city);
   }
 
   deleteCity(id: number): Observable<any> {
-    return this.http.delete(`${this.baseUrl}/delete-city/${id}`, { responseType: 'text' });
+    return this.http.delete(`${this.baseUrl}/${id}`, { responseType: 'text' });
   }
 
   getCity(id: number): Observable<Object> {
-    return this.http.get(`${this.baseUrl}/city/${id}`);
+    return this.http.get(`${this.baseUrl}/${id}`);
   }
 
   updateCity(id: number, value: any): Observable<Object> {
-    return this.http.post(`${this.baseUrl}/update-city/${id}`, value);
+    return this.http.post(`${this.baseUrl}/${id}`, value);
   }
 
 }
